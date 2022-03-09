12fe2b9472a9a073e0ced4c66892cb391c00b289
HaseFlrn <florianhase12@gmail.com>
2022-03-09 15:51:09 +0100

    snel automated build

---
+++ example/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Home from "@/components/Home.svelte";
+  import UserInput from "@/components/UserInput.svelte";
+  import { fade } from "svelte/transition";
+  import { Client } from "https://deno.land/x/mysql@v2.10.2/mod.ts";
+  let name = "";
+</script>
+
+<main transition:fade>
+  <Home {name} />
+
+  <p><br /></p>
+
+  <UserInput />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
